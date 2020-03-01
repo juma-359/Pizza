8d6bf872a0f3fddf3544bfa50b91f0f3392dde1d
juma-359 <joeljuma08@gmail.com>
2020-03-02 01:00:03 +0300

    addedfunctions to the pizza user order that will help keep the record of what the user selected and use it in charging him/her

--- js/script.js
+++ js/script.js
@@ -6,6 +6,25 @@ $(document).ready(function() {
     var pizzaCrustPrice = function() {
     var pizzaCrust = $("chose").value();
         return parseInt(pizzaCrust);
-    }
+    };
+
+    var pizzaToppingsPrice = function () {
+        var pizzaToppings = $("#flava").val();
+        return parseInt (pizzaToppings);
+    };
+
+    var pizzaQuantityPrice = function(){
+        var pizzaQuantity = $("#quantity").val();
+        return parseInt(pizzaQuantity);
+    };
 
 });
+
+var inputForUserOrder = new UserOrder (
+    pizzaSizePrice(),
+    pizzaCrustPrice(),
+    pizzaToppingsPrice(),
+    pizzaQuantityPrice()
+);
+
+
