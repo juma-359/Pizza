1865cfc4d514128d2cf20ab72ed7ea7da11dc7db
juma-359 <joeljuma08@gmail.com>
2020-03-01 23:34:03 +0300

    added functions to my pricing of the pizza size and crust to be responsive

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var pizzaSizePrice = function() {
+        var pizzaSize= $("#choose").val();
+        return parseInt(pizzaSize);
+    };
+    var pizzaCrustPrice = function() {
+    var pizzaCrust = $("chose").value();
+        return parseInt(pizzaCrust);
+    }
+
+});
