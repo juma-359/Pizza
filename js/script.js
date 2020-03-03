6893a0a02c6abd495e19c45c901407df0397a3bc
juma-359 <joeljuma08@gmail.com>
2020-03-03 17:56:08 +0300

    changes made

--- js/script.js
+++ js/script.js
@@ -1,69 +1,6 @@
-// $(document).ready(function() {
-
-//     var pizzaSizePrice = function() {
-//         var pizzaSize= $("#choose").val();
-//         return parseInt(pizzaSize);
-//     };
-//     var pizzaCrustPrice = function() {
-//     var pizzaCrust = $("chose").value();
-//         return parseInt(pizzaCrust);
-//     };
-
-//     var pizzaToppingsPrice = function () {
-//         var pizzaToppings = $("#flava").val();
-//         return parseInt(pizzaToppings);
-//     };
-
-//     var pizzaQuantityPrice = function(){
-//         var pizzaQuantity = $("#quantity").val();
-//         return parseInt(pizzaQuantity);
-//     };
-
-//     event.preventDefault();
-// });
-
-// var inputForUserOrder = new UserOrder (
-//     pizzaSizePrice(),
-//     pizzaCrustPrice(),
-//     pizzaToppingsPrice(),
-//     pizzaQuantityPrice()
-// );
-
-// var totalPrice = 
-// (inputForUserOrder.size + inputForUserOrder.crust + inputForUserOrder.toppings ) * inputForUserOrder.quantity;
-// if(totalPrice > 0) {
-//     alert("You spend total amount of ksh" + totalPrice);
-// }
-// else{
-//     alert("fill in all the form inputs to make an order.");
-// }
-
-// var delivery = confirm("for an delivery we charge additional ksh. 200 and we can deliver at your door step in few minutes");
-// if (delivery === true){
-//     for(;;){
-//         var location = prompt ("kindly let us know where to deliver for you your pizza")
-
-//     };
-// };
-// if (location !== ""){
-//     alert("your pizza will be delivered at " + location + " thank you for choosing us and always at your service");
-//     else if (location === false){
-
-//         alert("please to recieve your pittza enter a valid location");
-//     };
-//         alert("please to recieve your pittza enter a valid location");
-//     }
-// }
-// alert("Now your total amount is ksh" + " totalPrice" + " ksh 200")
-// else {
-//     alert("Thank you for choosing victorious pizzas we are here for you  you will pass by at our restaurant and pick your pizza");
-// };
-
 $(document).ready(function () {
     $("button#buy").click(function (event) {
-        
-        // alert("Your order is " + size + "" + topping + "" + crust + "and the total prize is" + totalCosts + "thank you for you delivery" );
-        function buyNow(size, crust, topping, quantity) {
+         function buyNow(size, crust, topping, quantity) {
             this.size = size;
             this.prize = 0;
             this.crust = crust;
@@ -81,15 +18,9 @@ $(document).ready(function () {
         var topping = $("#topping").val();
         var quantity = $("#quanty").val();
         var totalPrice = [];
-        // const totalPrice = (size + crust + topping + (quanty));
+       
         alert("You have ordered  ordered and the total is " + 7456 )
-        // var newPizzaOrder = new Placeorder (size, crust, topping);
-        // newPizzaOrder.cost();
-        // totalCosts.push(newPizzaOrder.price*quantity);
-        
-        // alert("Your order is " + size + "" + topping + "" + crust + "and the total prize is" + totalCosts)
-
-
+       
     });
 
       buyNow.prototype.Price = function(){
