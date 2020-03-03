0da93b083e3b740e85860c1c36ad4c9d9c195e14
juma-359 <joeljuma08@gmail.com>
2020-03-03 18:00:03 +0300

    changes in js that will allow my page to be responsive

--- js/script.js
+++ js/script.js
@@ -1,54 +1,93 @@
-$(document).ready(function() {
-    var pizzaSizePrice = function() {
-        var pizzaSize= $("#choose").val();
-        return parseInt(pizzaSize);
-    };
-    var pizzaCrustPrice = function() {
-    var pizzaCrust = $("chose").value();
-        return parseInt(pizzaCrust);
-    };
 
-    var pizzaToppingsPrice = function () {
-        var pizzaToppings = $("#flava").val();
-        return parseInt (pizzaToppings);
-    };
+$(document).ready(function () {
+    $("button#buy").click(function (event) {
+         function buyNow(size, crust, topping, quantity) {
+            this.size = size;
+            this.prize = 0;
+            this.crust = crust;
+            this.topping = topping;
+            this.quantity = quantity;
 
-    var pizzaQuantityPrice = function(){
-        var pizzaQuantity = $("#quantity").val();
-        return parseInt(pizzaQuantity);
-    };
+             size = ["small", "medium", "large"];
+             crust = ["Crisp", "Stuffed", "Cheese-Stuffed Crust", "Neapolitan Crust", "Thin Crust", "Pizza Bagels", "Sicilian Style", "Flatbread"];
+             topping = ["Green pepper", "Onions", "Saursage", "Bacon", "Extra cheese", "Black olives"];
+             quantity = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+        }
+        var size = $("#size").val();
+        var crust = $("#crust").val();
+        var topping = $("#topping").val();
+        var quantity = $("#quanty").val();
+        var totalPrice = [];
+       
+        alert("You have ordered  ordered and the total is " + 7456 )
+       
+    });
+
+      buyNow.prototype.Price = function(){
+          if(this.size === size[0]){
+              this.prize += 400;
+          }
+          else if(this.size === size[1]){
+              this.size += 600;
+          }
+          else if(this.size === size[2]){
+            this.size += 800;
+        }
+        if(this.crust === crust[0]){
+            this.crust += 200;
+        }
+        else if(this.crust === crust[1]){
+            this.crust += 250;
+        }
+        else if(this.crust === crust[2]){
+            this.size += 300;
+        }
+        else if(this.crust === crust[3]){
+            this.crust += 350;
+        }
+        else if(this.crust === crust[4]){
+            this.crust += 400;
+        }
+        else if(this.crust === crust[5]){
+            this.crust += 450;
+        }
+        else if(this.crust === crust[6]){
+            this.crust += 500;
+        }
+        else if(this.crust === crust[7]){
+            this.crust += 550;
+        }
+        if(this.topping === topping[0]){
+            this.topping += 200;
+        }
+        else if(this.topping === topping[1]){
+            this.topping += 250;
+        }
+        else if(this.topping === topping[2]){
+            this.topping += 300;
+        }
+        else if(this.topping === topping[3]){
+            this.topping += 350;
+        }
+        else if(this.topping === topping[4]){
+            this.topping += 400;
+        }
+        else if(this.topping === topping[5]){
+            this.topping += 450;
+        }
+        return this.price;
+      }
+     
+      buyNow.prototype.totalCost = function(){
+          var orderTotal = 0;
+          for(var order =0; order < totalCosts.length; order++){
+              orderTotal += totalCosts[order];
+          }
+          return orderTotal;
+      }
+
+
 
 });
-
-var inputForUserOrder = new UserOrder (
-    pizzaSizePrice(),
-    pizzaCrustPrice(),
-    pizzaToppingsPrice(),
-    pizzaQuantityPrice()
-);
-
-var totalPrice = 
-(inputForUserOrder.size + inputForUserOrder.crust + inputForUserOrder.toppings ) * inputForUserOrder.quantity;
-if(totalPrice > 0) {
-    alert("You spend total amount of ksh" + totalPrice);
-}
-
-
-var delivery = confirm("for an delivery we charge additional ksh. 200 and we can deliver at your door step in few minutes");
-if (delivery === true) {
-    for(;;){
-        var location = prompt ("kindly let us know where to deliver for you your pizza")
-        if (location !== "") {
-            alert("your pizza will be delivered at " + location + " thank you for choosing us and always at your service");
-            else {
-                alert("please to recieve your pittza enter a valid location")
-            };
-        };
-        alert("Now your total amount is ksh" + " totalPrice" + " ksh 200");
-        else {
-            alert("Thank you for choosing victorious pizzas we are here for you  you will pass by at our restaurant and pick your pizza");
-        }
-    };
-};
-
 
